39fbb7a82f03be076c0badd3b2355f2d340aac98
Paula Ruiz <paururo@gmail.com>
2022-07-31 21:50:51 +0200

    add read genes function

--- scripts/blast_analysis/run_blast.py
+++ scripts/blast_analysis/run_blast.py
@@ -6,6 +6,17 @@ from Bio.Seq import Seq
 import os
 import argparse
 
+def read_genes(genes_file:str):
+    '''
+    To store genes info of a file into a list
+    '''
+    list_genes = list()
+    with open(genes_file,'r') as in_file:
+        for line in in_file:
+            if '#' not in line:
+                list_genes.append(line.strip('\n').split('\t'))
+    return list_genes
+
 def main():
     parser = argparse.ArgumentParser(description = 'Script to blast proteins aa into a fasta nt')
     parser.add_argument('-g', dest = 'genes', required = True, help = 'file with gene coordinates')
@@ -13,6 +24,6 @@ def main():
     parser.add_argument('-p', dest = 'protein', required = True, help = 'file fasta to get proteins')
     parser.add_argument('-n', dest = 'jobname', required = True, help = 'name for job')
     args = parser.parse_args()
-
+    lista_genes = read_genes(args.genes) # list of genes to analyze
 if __name__ == '__main__':
     main()
