fee9c03f459e1f62d7d872e541d2030fd5db8449
Paula Ruiz <paururo@gmail.com>
2022-07-31 21:47:38 +0200

    add arguments

--- scripts/blast_analysis/run_blast.py
+++ scripts/blast_analysis/run_blast.py
@@ -7,6 +7,12 @@ import os
 import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description = 'Script to blast proteins aa into a fasta nt')
+    parser.add_argument('-g', dest = 'genes', required = True, help = 'file with gene coordinates')
+    parser.add_argument('-f', dest = 'fasta', required = True, help = 'file fasta to blast')
+    parser.add_argument('-p', dest = 'protein', required = True, help = 'file fasta to get proteins')
+    parser.add_argument('-n', dest = 'jobname', required = True, help = 'name for job')
+    args = parser.parse_args()
 
 if __name__ == '__main__':
     main()
