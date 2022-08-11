8e08099751e5f1a9d9e8cd9088a95e9ed8432299
PathoGenOmics <79568662+PathoGenOmics@users.noreply.github.com>
2022-08-11 12:48:07 +0200

    Update run_blast.py

--- scripts/blast_analysis/run_blast.py
+++ scripts/blast_analysis/run_blast.py
@@ -92,8 +92,8 @@ def write_threshold(jobname:str, number:int, lista_genes:list):
 def main():
     parser = argparse.ArgumentParser(description = 'Script to blast proteins aa into a fasta nt')
     parser.add_argument('-g', dest = 'genes', required = True, help = 'file with gene coordinates')
-    parser.add_argument('-f', dest = 'fasta', required = True, help = 'file fasta to blast')
-    parser.add_argument('-p', dest = 'protein', required = True, help = 'file fasta to get proteins')
+    parser.add_argument('-f', dest = 'protein', required = True, help = 'file fasta to blast')
+    parser.add_argument('-p', dest = 'fasta', required = True, help = 'file fasta to get proteins')
     parser.add_argument('-n', dest = 'jobname', required = True, help = 'name for job')
     args = parser.parse_args()
     lista_genes = read_genes(args.genes) # list of genes to analyze
