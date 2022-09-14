e98764c02b793f696c815c3f201be9f813df42d9
Paula Ruiz <paururo@gmail.com>
2022-09-14 13:19:27 +0200

    comands

--- scripts/duplicated_genes/command_mummer.sh
+++ scripts/duplicated_genes/command_mummer.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-run-mummer3 brumae.fasta brumae.genes.fa brumae_mummer -s -L 
+
+python3 clean_genes.py
+python3 multifasta.py -g clean_genes.tsv -f brumae_dnaA.fasta -j last_genes
+run-mummer3 brumae.fasta last_genes.genes.fa brumae_mummer -s -L 
+python3 process_mummer_out.py -f brumae_mummer.out -n result_mummer
